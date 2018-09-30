c36d5a650fc7c5afd0bb644289d0628419b01e3b
sheylert <sheylert@gmail.com>
2018-09-30 17:47:32 -0400

    commit

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const path = require('path')
 const bodyParser = require('body-parser')
 global.config = require('./config');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname,'public')))
@@ -12,13 +20,7 @@ app.use(express.static(path.join(__dirname,'public')))
 var routes = require('./routes');
 
 // congigurar CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+
 
 // rutas base
 app.use('/', routes);
